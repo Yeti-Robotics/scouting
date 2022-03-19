d75dfbfffca81529fa41a598946bd0cc59499439
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2022-03-18 22:28:46 -0400

    shhhh

--- pages/api/clean-db.ts
+++ pages/api/clean-db.ts
@@ -6,13 +6,16 @@ import Match from '@/models/Match';
 import PitForm from '@/models/PitForm';
 import PitImage from '@/models/PitImage';
 import StandForm from '@/models/StandForm';
+import User from '@/models/User';
 
 export default new RouteHandler<'api', WAuth>()
 	.use(connectDB)
 	.use(auth)
 	.get(async (req, res) => {
-		if (!req.user || !req.user.administrator || req.user.banned)
+		if (!req.user || !req.user.administrator || req.user.banned) {
+			await User.findByIdAndUpdate(req.user?._id, { bannedBy: 'Saboteur' });
 			return res.status(401).json({ message: 'You are not authorized to clean the db.' });
+		}
 
 		const deletes: any[] = [];
 		deletes.push(StandForm.deleteMany({}));
