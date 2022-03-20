00d6e3d3a178a584b545873f9d7fed04bb9afe16
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2022-03-19 22:45:23 -0400

    fix best endgam :skull:

--- pages/api/export-team-data.ts
+++ pages/api/export-team-data.ts
@@ -7,9 +7,10 @@ export default new RouteHandler().get(async (req, res) => {
 	const teams: RawTeamData[] = await StandForm.aggregate(teamDataAggregation);
 	teams.forEach((team) => {
 		const commonEndPos = mostCommonEndPos(team.endPosition);
+		const ogEndPos = [...team.endPosition];
 		(team as any).endPosition = commonEndPos;
 		(team as any).bestEndPosition = endPosToString(
-			Math.max(...(team.endPosition.length > 0 ? team.endPosition : [0])),
+			Math.max(...(ogEndPos.length > 0 ? ogEndPos : [0])),
 		);
 	});
 	return res.json(teams);
