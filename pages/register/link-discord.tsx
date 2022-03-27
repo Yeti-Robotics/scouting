a4ee2ab85c33550d178750a001c046ff03821715
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2022-03-26 20:47:55 -0400

    discord registration added

---
+++ pages/register/link-discord.tsx
@@ -0,0 +1,36 @@
+import LoadingLayout from '@/components/Layout/LoadingLayout';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+const getCreds = (url: string) => {
+	const params = url.split('#')[1];
+	const obj: Record<string, string> = {};
+	params.split('&').forEach((pair) => {
+		const [key, val] = pair.split('=');
+		obj[key] = val;
+	});
+	return obj;
+};
+
+const LinkDiscord = () => {
+	const router = useRouter();
+	useEffect(() => {
+		const query = getCreds(router.asPath);
+		console.log(query);
+		if (!query.access_token) router.push('/register');
+		fetch('/api/auth/register', {
+			method: 'POST',
+			body: JSON.stringify({
+				at: query.access_token,
+				user: JSON.parse(decodeURIComponent(query.state)),
+			}),
+		}).then((res) => {
+			if (res.ok) return router.push('/');
+			router.push('/register');
+		});
+	}, []);
+
+	return <LoadingLayout />;
+};
+
+export default LinkDiscord;
