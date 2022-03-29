7ed7d03d6701b9077e94223b0493310572a22e34
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2022-03-29 13:59:31 -0400

    removed ping command 💀 (it was for testing)

--- scouting-bot/commands/ping.ts
+++
@@ -1,7 +0,0 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { createCommand } from '../createCommand';
-
-export default createCommand({
-	command: new SlashCommandBuilder().setName('ping').setDescription('Replies with "Pong!"'),
-	execute: (interaction) => interaction.reply('Pong!'),
-});
