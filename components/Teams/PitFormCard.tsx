ed11d8fddb29a324d25d684689ea0fe734f72602
kdundigalla25 <kdundigalla25@gmail.com>
2023-02-11 16:24:24 -0500

    converted another page

--- components/Teams/PitFormCard.tsx
+++ components/Teams/PitFormCard.tsx
@@ -1,5 +1,6 @@
 import { TeamData } from '@/models/aggregations/teamData';
 import { PitFormI } from '@/models/PitForm';
+import { Box } from '@mantine/core';
 import Link from 'next/link';
 
 interface Props {
@@ -7,36 +8,14 @@ interface Props {
 	form: PitFormI;
 }
 
-const DataWrapper = styled.div`
-	display: flex;
-	flex-flow: row wrap;
-	justify-content: center;
-	width: 100%;
-
-	h4 {
-		margin: 1rem;
-	}
-`;
-
-const CardWrapper = styled.a`
-	margin: 0.5rem;
-	padding: 1rem;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	flex-grow: 1;
-	background-color: ${({ theme }) => theme.palette.primary.main};
-	border-radius: ${({ theme }) => theme.shape.borderRadius}px;
-`;
-
 const PitFormCard: React.VFC<Props> = ({ team, form }) => {
 	return (
 		<Link href={`/records/pit-forms/${form._id}`} passHref={true}>
-			<CardWrapper>
-				<DataWrapper>
+			<Box component='a'>
+				<Box>
 					<h4>scouter: {form.scouter}</h4>
-				</DataWrapper>
-			</CardWrapper>
+				</Box>
+			</Box>
 		</Link>
 	);
 };
