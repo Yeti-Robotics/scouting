553110a59490755d101ff7b71cca515a184bb6d2
kdundigalla25 <kdundigalla25@gmail.com>
2023-02-15 20:17:26 -0500

    fixed leaderboard

--- pages/casino/leaderboard.tsx
+++ pages/casino/leaderboard.tsx
@@ -2,7 +2,7 @@ import { Link } from '@/components/Link';
 import fetcher from '@/lib/fetch';
 import { useUser } from '@/lib/useUser';
 import { UserI } from '@/models/User';
-import { Box, Divider, Anchor, Loader } from '@mantine/core';
+import { Box, Divider, Anchor, Loader, Text, Title } from '@mantine/core';
 import useSWR from 'swr';
 
 const UserBets = () => {
@@ -16,25 +16,38 @@ const UserBets = () => {
 	return (
 		<>
 			<h1 style={{ marginBottom: 0 }}>Leaderboard</h1>
+			<Box
+				sx={{
+					textDecoration: 'none',
+					width: '100%',
+					pt: 2,
+					pb: 2,
+					display: 'flex',
+					justifyContent: 'space-around',
+				}}
+			>
+				<Title size={24}>Username</Title>
+				<Title size={24}>Coinage</Title>
+			</Box>
 			<Box sx={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
 				<Divider />
 				{data.map((user) => (
 					<>
-						<Link href={`/casino/users/${user._id}/bets`} passHref>
-							<Box
-								component='a'
-								sx={{
-									width: '100%',
-									pt: 2,
-									pb: 2,
-									display: 'flex',
-									justifyContent: 'space-around',
-								}}
-								key={user._id}
-							>
-								<Anchor component='div'>{user.username}</Anchor>
-								<Box sx={{ color: 'rgb(150, 150, 50)' }}>Coinage: {user.coins}</Box>
-							</Box>
+						<Link
+							p='md'
+							sx={{
+								textDecoration: 'none',
+								width: '100%',
+								pt: 2,
+								pb: 2,
+								display: 'flex',
+								justifyContent: 'space-around',
+							}}
+							href={`/casino/users/${user._id}/bets`}
+							passHref
+						>
+							<Anchor component='div'>{user.username}</Anchor>
+							<Box sx={{ color: 'rgb(150, 150, 50)' }}>Coinage: {user.coins}</Box>
 						</Link>
 						<Divider />
 					</>
