c98e5ed783590bfb77efaff472a0173c6fac01bd
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2023-03-04 13:55:17 -0500

    start enhanced match-data aggregation

--- components/Teams/CompareTeam.tsx
+++ components/Teams/CompareTeam.tsx
@@ -1,10 +1,30 @@
+import fetcher from '@/lib/fetch';
 import { RawTeamData } from '@/models/aggregations/teamData';
+import { MatchI } from '@/models/Match';
+import { ResponsiveScatterPlot, ScatterPlotRawSerie } from '@nivo/scatterplot';
 import {} from '@nivo/core';
+import useSWR from 'swr';
+import { Loader, Stack } from '@mantine/core';
 
 type Props = {
 	team: RawTeamData;
 };
 
 export const CompareTeam = ({ team }: Props) => {
-	return <div></div>;
+	const { data: matches } = useSWR<MatchI[]>(
+		`/api/team-data/${team.teamNumber}/matches`,
+		fetcher,
+	);
+
+	if (!matches) return <Loader size='xl' />;
+
+	const avgAutoData: ScatterPlotRawSerie<{ x: number; y: number }>[] = [
+		{ id: `Team ${team.teamNumber}`, data: [] },
+	];
+
+	return (
+		<Stack>
+			<ResponsiveScatterPlot data={avgAutoData} />
+		</Stack>
+	);
 };
