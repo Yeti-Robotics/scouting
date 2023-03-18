d8a92d60a69b2a2eb7e10654801cd708b2e028e4
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2023-03-18 10:53:34 -0400

    finish the verify tba

--- pages/records/verify-forms/tba.tsx
+++ pages/records/verify-forms/tba.tsx
@@ -2,20 +2,65 @@ import fetcher from '@/lib/fetch';
 import { aggregatePiecesScored } from '@/lib/matchDataUtils';
 import { useUser } from '@/lib/useUser';
 import { MatchWForms } from '@/models/aggregations/matchWForms';
-import { Card, Group, Loader, Stack, Title } from '@mantine/core';
+import { Card, Group, Loader, Paper, Stack, Text, Title } from '@mantine/core';
 import { useIsomorphicEffect } from '@mantine/hooks';
-import { useState } from 'react';
 import useSWR from 'swr';
 
 const MatchCard = ({ match }: { match: MatchWForms }) => {
-	const [errors, setErrors] = useState<string[]>([]);
-	const { blue, red } = aggregatePiecesScored(match);
+	const blueErrors: string[] = [];
+	const redErrors: string[] = [];
 
-	// Match is guaranteed to have official prop if this component is rendered
+	// Our data
+	const { blue, red } = aggregatePiecesScored(match);
+	// TBA data, guaranteed to have official prop if this component is rendered
+	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+	const { blue: tbaBlue, red: tbaRed } = match.official!;
+
+	// ============= Blue Validations =============
+	blue.highCubes !== tbaBlue.highCubes && blueErrors.push('Blue Cubes High');
+	blue.highCones !== tbaBlue.highCones && blueErrors.push('Blue Cones High');
+
+	blue.midCubes !== tbaBlue.midCubes && blueErrors.push('Blue Cubes Mid');
+	blue.midCones !== tbaBlue.midCones && blueErrors.push('Blue Cones Mid');
+
+	blue.lowCubes !== tbaBlue.lowCubes && blueErrors.push('Blue Cubes Low');
+	blue.lowCones !== tbaBlue.lowCones && blueErrors.push('Blue Cones Low');
+
+	// ============= Red Validations =============
+	red.highCubes !== tbaRed.highCubes && redErrors.push('Blue Cubes High');
+	red.highCones !== tbaRed.highCones && redErrors.push('Blue Cones High');
+
+	red.midCubes !== tbaRed.midCubes && redErrors.push('Blue Cubes Mid');
+	red.midCones !== tbaRed.midCones && redErrors.push('Blue Cones Mid');
+
+	red.lowCubes !== tbaRed.lowCubes && redErrors.push('Blue Cubes Low');
+	red.lowCones !== tbaRed.lowCones && redErrors.push('Blue Cones Low');
 
 	return (
-		<Card withBorder shadow='md'>
-			<Title>{match.matchNumber}</Title>
+		<Card withBorder shadow='xl'>
+			<Title align='center'>Match {match.matchNumber}</Title>
+			<Group align='flex' position='center'>
+				<Paper bg='blue' p='xs' withBorder>
+					<Stack>
+						<Title order={2}>Blue</Title>
+						{blueErrors.length === 0 ? (
+							<Text>No Errors 😁</Text>
+						) : (
+							blueErrors.map((err, i) => <Text key={i}>{err}</Text>)
+						)}
+					</Stack>
+				</Paper>
+				<Paper bg='red' p='xs' withBorder>
+					<Stack>
+						<Title order={2}>Red</Title>
+						{redErrors.length === 0 ? (
+							<Text>No Errors 😁</Text>
+						) : (
+							redErrors.map((err, i) => <Text key={i}>{err}</Text>)
+						)}
+					</Stack>
+				</Paper>
+			</Group>
 		</Card>
 	);
 };
