be29fc949c87105064329736cf9973df9d6536c8
drew-beamer <drbeamer@davidson.edu>
2024-01-27 15:12:33 -0500

    minor fixes for tailwind

--- app/spr/recomputeButton.tsx
+++ app/spr/recomputeButton.tsx
@@ -3,6 +3,7 @@
 function RecomputeButton() {
 	return (
 		<button
+			className='bg-slate-500 border-red-100 border-1 dark:bg-red-500'
 			onClick={() => {
 				fetch('/api/spr/recompute', { method: 'POST' });
 			}}
