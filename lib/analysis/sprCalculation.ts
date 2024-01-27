ebcc306a51816ee03db92a1c90c80223cea6ac58
kdundigalla25 <kdundigalla25@gmail.com>
2024-01-26 19:49:30 -0500

    add functions for spr calculations

--- lib/analysis/sprCalculation.ts
+++ lib/analysis/sprCalculation.ts
@@ -1,14 +1,13 @@
-const testData = {
-	frc3506: ['scout1', 'scout2'],
-	frc2485: ['scout3', 'scout4'],
-	frc4290: ['scout5', 'scout6'],
-};
-
-const getScoutCombinations = <ValidScoutT extends string>(data: {
-	[K in keyof typeof testData]: ValidScoutT[];
-}) => {
-	const scoutTeams = Object.values(data);
-	const combinations = [];
+/**
+ * Takes the scouters for each match and finds all valid combinations
+ * @param allianceScouts: object mapping teams to list of scouter usernames
+ * @returns combinations
+ */
+const getScoutCombinations = <ValidScoutT extends string, ValidTeamT extends string>(
+	allianceScouts: Record<ValidTeamT, ValidScoutT[]>,
+) => {
+	const scoutTeams = Object.values(allianceScouts) as ValidScoutT[][];
+	const combinations: ValidScoutT[][] = [];
 
 	for (let i = 0; i < 8; i++) {
 		combinations.push([
@@ -21,15 +20,13 @@ const getScoutCombinations = <ValidScoutT extends string>(data: {
 	return combinations;
 };
 
-const scoutScores = {
-	scout1: 15,
-	scout2: 15,
-	scout3: 7,
-	scout4: 30,
-	scout5: 18,
-	scout6: 12,
-};
-
+/**
+ * Finds average error for each scouter based on the alliances they are a part of
+ * @param scores: object mapping usernames to reported score
+ * @param combinations
+ * @param tbaScore: total alliance score reported by tba
+ * @returns averageError: object mapping username to average error
+ */
 const avgError = <ValidScoutT extends string>(
 	scores: Record<ValidScoutT, number>,
 	combinations: ValidScoutT[][],
@@ -57,3 +54,40 @@ type MatchData<T extends string> = {
 	[K in keyof typeof testData]: T[]
 }
 */
+
+/**
+ * Calculate's each scouter's expected contribution to match's error
+ * @param scores: object mapping usernames to reported score
+ * @param allianceScouts: object mapping teams to list of scouter usernames
+ * @param tbaScore: total alliance score reported by tba
+ * @returns expectedContribution: object mapping each username to their expected contribution to match's error
+ */
+const scoutExpectedContribution = <ValidScoutT extends string, ValidTeamT extends string>(
+	scores: Record<ValidScoutT, number>,
+	allianceScouts: Record<ValidTeamT, ValidScoutT[]>,
+	tbaScore: number,
+) => {
+	const combinations = getScoutCombinations<ValidScoutT, ValidTeamT>(allianceScouts);
+	const averageError = avgError<ValidScoutT>(scores, combinations, tbaScore);
+	const expectedContribution = {} as Record<ValidScoutT, number>;
+
+	combinations.forEach((combo) => {
+		const actualError = Math.abs(
+			tbaScore - scores[combo[0]] - scores[combo[1]] - scores[combo[2]],
+		);
+		combo.forEach((scoutX, index) => {
+			const scoutY = combo[(index + 2) % 3];
+			const scoutZ = combo[(index + 1) % 3];
+			const scoutXExpectedError = Math.abs(
+				actualError - (averageError[scoutY] + averageError[scoutZ]) / 3,
+			);
+			expectedContribution[scoutX] = expectedContribution[scoutX]
+				? expectedContribution[scoutX] + scoutXExpectedError / 4
+				: scoutXExpectedError / 4;
+		});
+	});
+
+	return expectedContribution;
+};
+
+export default scoutExpectedContribution;
