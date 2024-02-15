91596c3cacfd758091acbc295cf60ae7c8343848
drew-beamer <drbeamer@davidson.edu>
2024-02-14 23:36:42 -0500

    minor styling fixes noticed

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
 			},
 		},
 		extend: {
+			fontFamily: {
+				sans: ['var(--font-os)'],
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
