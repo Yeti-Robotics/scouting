3ec669874c06cb731ef941554c9b38fc8efa4048
drew-beamer <drbeamer@davidson.edu>
2024-02-14 23:37:42 -0500

    styling cleanup

--- app/picklist/page.tsx
+++ app/picklist/page.tsx
@@ -5,38 +5,44 @@ import { TeamAvgsI, PickabilityWeightsI } from '@/lib/types/Pickability';
 import { avgDataPipeline } from '@/models/aggregations/averageData';
 import { firstPickWeights, secondPickWeights } from '../../lib/analysis/pickability-weights';
 import { connectToDbB } from '@/middleware/connect-db';
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-  
+import Link from 'next/link';
 
 export default async function PicklistPage() {
-    await connectToDbB();
-    const picklists = await PickList.find({});
-    console.log(picklists)
-    return 		<main className='mx-auto max-w-[540px]'>
-   {picklists.map((picklist)=> {
-    return <Card>
-      <Card>
-  <CardHeader>
-    <CardTitle>{picklist.name}</CardTitle>
-    <CardDescription>
-      <p>Updated at: {picklist.updatedAt.toDateString()}</p>
-    </CardDescription>
-  </CardHeader>
-  <CardContent>
-    <Button variant="outline">View</Button>
-  </CardContent>
-</Card>
-
-    </Card>
-    
-   })}
-</main>
+	await connectToDbB();
+	const picklists = await PickList.find({});
+	console.log(picklists);
+	return (
+		<main className='mx-auto mt-12 max-w-[540px]'>
+			<h1 className='typography mb-6'>YETI Picklists</h1>
+			<div className='grid grid-cols-1 gap-5'>
+				{picklists
+					.sort((a, b) => (b.updatedAt > a.updatedAt ? 1 : -1))
+					.map((picklist) => {
+						return (
+							<Card>
+								<CardHeader>
+									<div className='flex'>
+										<div className='grow'>
+											<CardTitle className='duration-250 transition-colors hover:text-primary'>
+												<Link href={`/picklist/${picklist._id.toString()}`}>
+													{picklist.name}
+												</Link>
+											</CardTitle>
+											<CardDescription>
+												Updated • {picklist.updatedAt.toDateString()}
+											</CardDescription>
+										</div>
+										<Link href={`/picklist/${picklist._id.toString()}`}>
+											<Button variant='link'>View</Button>
+										</Link>
+									</div>
+								</CardHeader>
+							</Card>
+						);
+					})}
+			</div>
+		</main>
+	);
 }
