a480fc44926a1fa4ab0f5fb286757dfd6febd832
drew-beamer <drbeamer@davidson.edu>
2024-02-16 00:51:43 -0500

    clean up and refactor

--- app/picklist/[slug]/page.tsx
+++ app/picklist/[slug]/page.tsx
@@ -1,11 +1,12 @@
-import PickList from '@/models/PickList';
+import PickList, { PickListI } from '@/models/PickList';
 import PickListTable from '@/components/PicklistTable';
-import mongoose from 'mongoose';
 import StandForm from '@/models/StandForm';
+import TeamContextProvider from './team-context-provider';
 import { avgDataPipeline } from '@/models/aggregations/averageData';
 import { connectToDbB } from '@/middleware/connect-db';
 import { firstPickWeights, secondPickWeights } from '@/lib/analysis/pickability-weights';
-import { PickabilityWeightsI, TeamAvgsI } from '@/lib/types/Pickability';
+import { PickabilityWeightsI, TeamAvgsI, TeamDerivedStatsI } from '@/lib/types/Pickability';
+import { UpdateButton } from '../crud-components';
 
 /**
  * Computes pickability using specified weights -- essentially computes a weighted sum
@@ -28,6 +29,52 @@ function computeTeamPickability(team: TeamAvgsI, weights: PickabilityWeightsI) {
 	}, 0);
 }
 
+/**
+ * Retrieves the averages of team data.
+ * @returns {Promise<TeamAvgsI[]>} A promise that resolves to an array of team averages.
+ */
+async function getDerivedStatistics(): Promise<TeamDerivedStatsI[]> {
+	return await StandForm.aggregate<TeamAvgsI>(avgDataPipeline).then((res) =>
+		res.map((team) => ({
+			...team,
+			firstPickability: computeTeamPickability(team, firstPickWeights),
+			secondPickability: computeTeamPickability(team, secondPickWeights),
+			climbRate: 100 * (team.climbSuccess / team.forms),
+		})),
+	);
+}
+
+async function getPicklist(_id: string): Promise<PickListI | null> {
+	return await PickList.findById(_id).exec();
+}
+
+/**
+ * Generates an empty row.
+ * @param _id - The team id
+ * @returns An object representing a team with no data collected yet.
+ */
+function generateEmptyRow(_id: number): TeamDerivedStatsI {
+	return {
+		_id: _id,
+		autoAmpNotes: 0,
+		autoSpeakerNotes: 0,
+		teleopAmpNotes: 0,
+		teleopSpeakerNotes: 0,
+		teleopAmplifiedSpeakerNotes: 0,
+		trapNotes: 0,
+		climbRate: 0,
+		firstPickability: 0,
+		secondPickability: 0,
+		forms: 0,
+		climbSuccess: 0,
+		defense: 0,
+	};
+}
+
+/**
+ * Generates static parameters for the page.
+ * @returns The array of slugs, representing Picklist _ids.
+ */
 export async function generateStaticParams() {
 	await connectToDbB();
 	const picklists = await PickList.find({}, { _id: 1 });
@@ -35,41 +82,49 @@ export async function generateStaticParams() {
 	return slugs;
 }
 
+/**
+ * Renders the PicklistPage component.
+ *
+ * @param params - The parameters object containing the slug.
+ * @param params.slug - The slug used to fetch the picklist.
+ * @returns The rendered PicklistPage component.
+ */
 export default async function PicklistPage({ params }: { params: { slug: string } }) {
 	await connectToDbB();
-	const objectId = new mongoose.Types.ObjectId(params.slug);
-	const averages = await StandForm.aggregate<TeamAvgsI>(avgDataPipeline).then((res) =>
-		res
-			.map((team) => ({
-				...team,
-				firstPickability: computeTeamPickability(team, firstPickWeights),
-				secondPickability: computeTeamPickability(team, secondPickWeights),
-				climbRate: 100 * (team.climbSuccess / team.forms),
-			}))
-			.sort((b, a) => a.firstPickability - b.firstPickability),
-	);
-	const picklist = await PickList.findById(objectId).exec();
-
-	if (picklist && averages) {
-		type TeamNumber = number;
-		type TeamIndexMap = Record<TeamNumber, number>;
-		const {
-			name,
-			ordering,
-			updatedAt,
-		}: { name: string; ordering: TeamNumber[]; updatedAt: Date } = picklist;
-		const parsedPicklist = { name, ordering, updatedAt: updatedAt.toDateString() };
-		const indices: TeamIndexMap = {};
-		ordering.forEach((team, index) => (indices[team] = index));
-		averages.sort((a, b) => indices[a._id as TeamNumber] - indices[b._id as TeamNumber]);
-		return (
-			<main className='mx-auto ml-4 mt-16 p-6'>
-				<h1 className='typography mb-4 text-primary'>{name}</h1>
-				<p className='lead mb-6'>Last Updated • {parsedPicklist.updatedAt}</p>
-				<PickListTable data={averages} picklist={parsedPicklist} />
-			</main>
-		);
-	} else {
-		return <div>picklist not found</div>;
+	// Retrieve the derived statistics and picklist
+	let derivedStatistics = await getDerivedStatistics();
+	const picklist = await getPicklist(params.slug);
+	// Return error if problem fetching data
+	if (!picklist || !derivedStatistics) {
+		return <div>Error fetching data, or picklist does not exist</div>;
 	}
+	const { name, ordering, updatedAt } = picklist;
+	// Create a map to store the indices of teams, constant-time lookup reduces time complexity
+	// of sorting derived statistics
+	const indices: Map<number, number> = new Map();
+	// Create a set to store the teams with data
+	const teamsWithData = new Set(derivedStatistics.map(({ _id }) => _id));
+	// Iterate through the ordering and populate the indices map
+	ordering.forEach((_id, index) => {
+		indices.set(_id, index);
+		// If a team does not have data, generate an empty row for it
+		if (!teamsWithData.has(_id)) {
+			derivedStatistics.push(generateEmptyRow(_id));
+		}
+	});
+	// Filter and sort the derived statistics based on the ordering
+	derivedStatistics = derivedStatistics
+		.filter((team) => ordering.indexOf(team._id) > -1)
+		.sort((a, b) => (indices.get(a._id) as number) - (indices.get(b._id) as number));
+	// Render the PicklistPage component
+	return (
+		<main className='mx-auto mt-16 p-6'>
+			<h1 className='typography mb-4 text-primary'>{name}</h1>
+			<p className='lead mb-6'>Last Updated • {updatedAt.toDateString()}</p>
+			<TeamContextProvider initialState={derivedStatistics}>
+				<UpdateButton mongoId={params.slug} />
+				<PickListTable />
+			</TeamContextProvider>
+		</main>
+	);
 }
