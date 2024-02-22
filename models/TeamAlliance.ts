c4f639ec3082c2cf47757885e0af4ef74a493ad3
drew-beamer <drbeamer@davidson.edu>
2024-02-21 19:27:18 -0500

    add alliance number to model

--- models/TeamAlliance.ts
+++ models/TeamAlliance.ts
@@ -3,11 +3,13 @@ import { model, models, Schema } from 'mongoose';
 const teamAllianceSchema = new Schema<TeamAllianceI>({
 	compKey: { type: String, required: true, ref: 'compKey' },
 	team_number: { type: Number, required: true, ref: 'team' },
+	alliance_number: { type: Number, required: true },
 });
 
 export interface TeamAllianceI {
 	compKey: string;
 	team_number: number;
+	alliance_number: number;
 }
 
 const TeamAlliance = models?.teamAlliance || model('teamAlliance', teamAllianceSchema);
