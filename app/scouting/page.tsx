c76f13add726ffd0a9dc23d19287675f7ae5953e
drew-beamer <drbeamer@davidson.edu>
2024-02-23 13:20:24 -0500

    have page render main/h1, change tabs -> standform

--- app/scouting/page.tsx
+++ app/scouting/page.tsx
@@ -1,5 +1,10 @@
-import Tabs from '@/components/Forms/StandForm/Tabs';
+import StandForm from '@/components/Forms/StandForm';
 
-export default function StandForm() {
-	return <Tabs />;
+export default function StandFormPage() {
+	return (
+		<main className='mx-auto flex w-full max-w-[360px] flex-wrap items-center justify-center py-8'>
+			<h1 className='typography mb-6 w-full'>Stand Form</h1>
+			<StandForm create canEdit />
+		</main>
+	);
 }
