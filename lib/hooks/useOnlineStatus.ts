e4737b70581c73a29a1024b950f38e021d8a0dd5
drew-beamer <drbeamer@davidson.edu>
2024-02-23 13:19:49 -0500

    create hook to listen to online/offline status

---
+++ lib/hooks/useOnlineStatus.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+
+export function useOnlineStatus() {
+	const [isOnline, setIsOnline] = useState(true);
+
+	useEffect(() => {
+		const handleOnline = () => setIsOnline(true);
+		const handleOffline = () => setIsOnline(false);
+		window.addEventListener('online', handleOnline);
+		window.addEventListener('offline', handleOffline);
+		return () => {
+			window.removeEventListener('online', handleOnline);
+			window.removeEventListener('offline', handleOffline);
+		};
+	}, []);
+
+	return isOnline;
+}
