efac16ccf072adfc474942c1cfe369dd8c1f2afc
drew-beamer <drbeamer@davidson.edu>
2024-02-25 14:46:30 -0500

    add basic TBA util types

---
+++ lib/types/tba/utilTypes.ts
@@ -0,0 +1,4 @@
+export type TBATeamKey = `frc${number}`;
+export type TBACompLevel = 'qm' | 'qf' | 'sf' | 'f';
+export type TBAVideo = { key: string; type: string };
+export type TBAEventKey = `${number}${string}`;
