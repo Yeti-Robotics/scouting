3cff5fc7cdcec24959a7883f339ff73a30faa331
Sam Perlmutter <sam.perlmutter227@gmail.com>
2024-03-07 19:39:24 -0500

    Enable PWA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,12 +9,16 @@ export default class _Document extends Document {
 	render() {
 		return (
 			<Html>
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
+				<Head>
+					<link rel="manifest" href="/manifest.json" />
+					<link rel="apple-touch-icon" href="/icon.png"></link>
+					<meta name="theme-color" content="#fff" />
+				</Head>
+				 <body>
+					  <Main />
+					  <NextScript />
+				 </body>
 			</Html>
-		);
+	);
 	}
-}
+	}
