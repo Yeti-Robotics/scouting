ec9536adebe7f5a3e5092ff1a21e285146ff467f
kdundigalla25 <kdundigalla25@gmail.com>
2024-03-16 08:42:16 -0400

    add clear stand form api route

---
+++ pages/api/clear-stand-forms.ts
@@ -0,0 +1,17 @@
+import { RouteHandler } from '@/lib/api/RouteHandler';
+import { WAuth } from '@/lib/api/types';
+import { auth } from '@/middleware/auth';
+import connectDB from '@/middleware/connect-db';
+import StandForm from '@/models/StandForm';
+
+export default new RouteHandler<'api', WAuth>()
+	.use(connectDB)
+	.use(auth)
+	.get(async (req, res) => {
+		if (!req.user.administrator || !req.user || req.user.banned)
+			return res.status(401).json({ message: 'You are not authorized to clear stand forms.' });
+
+        await StandForm.deleteMany();
+
+		return res.status(200).json({ message: 'DB successfully cleaned out.' });
+	});
